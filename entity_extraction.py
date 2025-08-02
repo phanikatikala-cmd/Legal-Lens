718aa3250557985885da2903fdb45378fa110484
phanikatikala-cmd <phanikatikala@gmail.com>
2025-08-02 12:29:44 +0530

    Delete entity_extraction.py

--- entity_extraction.py
+++
@@ -1,7 +0,0 @@
-from transformers import pipeline
-
-ner_pipeline = pipeline("ner", model="dslim/bert-base-NER", aggregation_strategy="simple")
-
-def extract_entities(text):
-    entities = ner_pipeline(text)
-    return [{"entity": ent['entity_group'], "word": ent['word'], "score": round(ent['score'], 2)} for ent in entities]
