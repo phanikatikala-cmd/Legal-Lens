8d98a24bd79ccd9358fec2979e610d8a723c08d9
phanikatikala-cmd <phanikatikala@gmail.com>
2025-08-02 12:28:56 +0530

    Delete clause_simplification.py

--- clause_simplification.py
+++
@@ -1,7 +0,0 @@
-# clause_simplification.py
-from transformers import pipeline
-
-simplify_pipeline = pipeline("summarization", model="facebook/bart-large-cnn")
-
-def simplify_clause(text):
-    return simplify_pipeline(text, max_length=100, min_length=30, do_sample=False)[0]['summary_text']
